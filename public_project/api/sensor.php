fab0ba7b5d3a769043adedc54239e589db918374
sanjlal <42592889+sanjlal@users.noreply.github.com>
2018-10-24 18:52:34 -0400

    Modifying arguments

--- public_project/api/sensor.php
+++ public_project/api/sensor.php
@@ -5,8 +5,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
+$siteId = intval($_GET['siteId'] ?? 0);
+//echo $turbineDeployedId;
+if ($siteId < 1) {
+  throw new Exception('Invalid Task ID');
+}
 // 1. Go to the database and get all work associated with the $taskId
-$commentArr = Sensor::fetchAll();
+$commentArr = Sensor::fetchAll($siteId);
 // 2. Convert to JSON
 $json = json_encode($commentArr, JSON_PRETTY_PRINT);
 // 3. Print
