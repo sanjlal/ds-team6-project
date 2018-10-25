191bbb47cb896b6831686cba3675249e9bab4c05
sanjlal <42592889+sanjlal@users.noreply.github.com>
2018-10-24 23:55:35 -0400

    Modifying arguments- sensorDeployed

--- public_project/api/sensorDeployed.php
+++ public_project/api/sensorDeployed.php
@@ -8,9 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $sensorId = intval($_GET['sensorId '] ?? 0);
 $turbineDeployedId=intval($_GET['turbineDeployedId '] ?? 0);
 //echo $turbineDeployedId;
-if ($siteId < 1) {
-  throw new Exception('Invalid Task ID');
-}
+
 // 1. Go to the database and get all work associated with the $taskId
 $commentArr = SensorDeployed::fetchAll($siteId,$turbineDeployedId);
 // 2. Convert to JSON
